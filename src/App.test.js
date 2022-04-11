55e3b511d811a2b4e7853609172f2a09b383fec8
tanuj <tanujkathuria.kathuria@gmail.com>
2022-04-11 16:29:59 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,12 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/earn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn  link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/earn react/i);
   expect(linkElement).toBeInTheDocument();
 });
